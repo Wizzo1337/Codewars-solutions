a2fbc3bbad5e2d6b28bf65725847c110d63eb839
Thomas Willson <thomaswilllson@yahoo.com>
2022-09-26 20:16:53 -0700

    Codewars solution for 9-26-22

---
+++ 7-kyu/Coding-Meetup-#4-Higher-Order-Functions-Series-Find-the-first-Python-developer/main.js
@@ -0,0 +1,39 @@
+// You will be given an array of objects (associative arrays in PHP) representing data about developers who have signed up to attend the next coding meetup that you are organising. The list is ordered according to who signed up first.
+
+// Your task is to return one of the following strings:
+
+//     < firstName here >, < country here > of the first Python developer who has signed up; or
+//     There will be no Python developers if no Python developer has signed up.
+
+// For example, given the following input array:
+
+// var list1 = [
+//   { firstName: 'Mark', lastName: 'G.', country: 'Scotland', continent: 'Europe', age: 22, language: 'JavaScript' },
+//   { firstName: 'Victoria', lastName: 'T.', country: 'Puerto Rico', continent: 'Americas', age: 30, language: 'Python' },
+//   { firstName: 'Emma', lastName: 'B.', country: 'Norway', continent: 'Europe', age: 19, language: 'Clojure' }
+// ];
+
+// your function should return Victoria, Puerto Rico.
+
+// Notes:
+
+//     The input array will always be valid and formatted as in the example above.
+
+
+
+
+// This kata is part of the Coding Meetup series which includes a number of short and easy to follow katas which have been designed to allow mastering the use of higher-order functions. In JavaScript this includes methods like: forEach, filter, map, reduce, some, every, find, findIndex. Other approaches to solving the katas are of course possible.
+
+function getFirstPython(list) {
+  return list.find(ele => ele.language === 'Python') ? `${list.find(ele => ele.language === 'Python').firstName}, ${list.find(ele => ele.language === 'Python').country}` : 'There will be no Python developers'
+}
+
+console.log(getFirstPython([
+  { firstName: 'Mark', lastName: 'G.', country: 'Scotland', continent: 'Europe', age: 22, language: 'JavaScript' },
+  { firstName: 'Victoria', lastName: 'T.', country: 'Puerto Rico', continent: 'Americas', age: 30, language: 'Python' },
+  { firstName: 'Emma', lastName: 'B.', country: 'Norway', continent: 'Europe', age: 19, language: 'Clojure' }
+]), 'Victoria, Puerto Rico');
+console.log(getFirstPython([
+  { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 29, language: 'JavaScript' },
+  { firstName: 'Amar', lastName: 'V.', country: 'Bosnia and Herzegovina', continent: 'Europe', age: 32, language: 'Ruby' },
+]), 'There will be no Python developers');
