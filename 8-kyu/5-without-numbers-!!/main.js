c299313b524a8139eb987bbc82e07b6c4566520c
Thomas Willson <thomaswilllson@yahoo.com>
2023-01-22 21:12:05 -0800

    Codewars solution for 1-22-23

---
+++ 8-kyu/5-without-numbers-!!/main.js
@@ -0,0 +1,12 @@
+// Write a function that always returns 5
+
+// Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+// Good luck :)
+
+function unusualFive() {
+  let arr = ['a', 'b', 'c', 'd', 'e', 'f']
+  return arr.indexOf('f')
+}
+
+console.log(unusualFive())
